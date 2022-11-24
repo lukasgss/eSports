e54177ae3d4f66fde7a3b33da08bd09d884dfe11
Lucas Gomes <lucas@pop-os.localdomain>
2022-11-24 14:16:37 -0300

    remoçao imports nao usados

--- src/pages/Player/Home/components/ListagemCampeonatos.tsx
+++ src/pages/Player/Home/components/ListagemCampeonatos.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
-import { IoIosArrowUp } from "react-icons/io";
+import React, { useRef } from "react";
 import { useForm } from "react-hook-form";
 import Dropdown, {
   DadosDropdown,
